20b4d29cdb9beac72e417922415cd3f930e755c9
李南海 <nanhai.li@163.com>
2017-08-06 17:58:24 +0800

    2017-8-6 17:58

--- src/main/java/com/lnh/controller/EmpController.java
+++ src/main/java/com/lnh/controller/EmpController.java
@@ -1,33 +1,40 @@
 package com.lnh.controller;
 
-import java.util.List;
-
+import com.lnh.bean.EmpBean;
+import com.lnh.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.lnh.bean.EmpBean;
-import com.lnh.service.EmpService;
+import java.util.List;
 
 /**
  * Created by linanhai on 2017/8/2.
  */
 @Controller
-@RequestMapping(value="/emp",method={RequestMethod.POST,RequestMethod.GET})
+@RequestMapping(value = "/emp")
 public class EmpController {
 
     @Autowired
     private EmpService service;
 
-    @RequestMapping("/getEmp")
-    public String getEmp(Model model,@RequestParam(value="condition", required=false)
-            String condition){
-        model.addAttribute("emps",service.getEmp(condition));
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
         return "emp";
+    }
+
+    @RequestMapping(value = "/getEmp", method = {RequestMethod.POST, RequestMethod.GET})
+    @ResponseBody
+    public List<EmpBean> getEmp(String condition) {
+        try {
+            return service.getEmp(condition);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
