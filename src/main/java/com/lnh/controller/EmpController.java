982c56d6aa61513900dc3d0ec0c521a60b614704
李南海 <nanhai.li@163.com>
2017-08-06 20:39:53 +0800

    2017-8-6 20:39

--- src/main/java/com/lnh/controller/EmpController.java
+++ src/main/java/com/lnh/controller/EmpController.java
@@ -3,19 +3,15 @@ package com.lnh.controller;
 import com.lnh.bean.EmpBean;
 import com.lnh.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 /**
  * Created by linanhai on 2017/8/2.
  */
-@Controller
+@RestController
 @RequestMapping(value = "/emp")
 public class EmpController {
 
@@ -23,13 +19,14 @@ public class EmpController {
     private EmpService service;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model) {
-        return "emp";
+    public ModelAndView index() {
+
+        return new ModelAndView("emp");
     }
 
     @RequestMapping(value = "/getEmp", method = {RequestMethod.POST, RequestMethod.GET})
-    @ResponseBody
     public List<EmpBean> getEmp(String condition) {
+
         try {
             return service.getEmp(condition);
         } catch (Exception e) {
